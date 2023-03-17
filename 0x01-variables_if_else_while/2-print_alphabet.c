4f37a1e65242625ba04fe1ce8df069022231f445
Manalhub <manalrahali24@gmail.com>
2023-03-17 13:04:53 -0700

    0x01. C - Variables, if, else, while Task 0,1,2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+{
+             char j;
+             j = ‘a’;
+             while (j <= ‘z’)
+             {
+                    putchar(c);
+                    c++;
+              }
+             putchar('\n'); 
+             return (0);
+}

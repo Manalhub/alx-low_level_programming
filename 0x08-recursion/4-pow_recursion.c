f54e1496d4f0da7ce48fe11dd4a4023cf64b942a
Manalhub <manalrahali24@gmail.com>
2023-04-06 05:55:41 -0700

    updare

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,17 +7,18 @@
  *
  * Return: result of the power
  */
-
-int _pow_recursion(int x, int y) 
+int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-	{
-		return -1;
-	}
-	else if (y > 0)
-	{
-		return (x * _pow_recursion(x, y-1));
-	}
-	else if (y == 0)
-		return 1;
+	        if (y < 0)
+			        {
+					                return -1;
+							        }
+		        else if (y > 0)
+				        {
+						                return (x * _pow_recursion(x, y-1));
+								        }
+			        else // y == 0
+					        {
+							                return 1;
+									        }
 }

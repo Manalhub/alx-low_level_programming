9adf8dcd1e47d0a3735720fa185d15b3036f8507
Manalhub <manalrahali24@gmail.com>
2023-04-06 15:06:50 -0700

    update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _nat_sqrt(int n, int i)
+int _nat_sqrt(int n, int i);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 #endif

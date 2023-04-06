21c9f327e2fe493aaee08201d09f856927b2d75c
Manalhub <manalrahali24@gmail.com>
2023-04-06 05:57:32 -0700

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,7 +17,7 @@ int _pow_recursion(int x, int y)
 				        {
 						                return (x * _pow_recursion(x, y-1));
 								        }
-			        else // y == 0
+			        else
 					        {
 							                return 1;
 									        }

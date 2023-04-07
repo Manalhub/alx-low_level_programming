c2743da6920f05623b2785f735ed339c7d507a1b
Manalhub <manalrahali24@gmail.com>
2023-04-07 06:31:19 -0700

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,9 +6,9 @@
  * @s2: pointer to string prmtrs
  * Return: 0 or 1
  */
-int wildcmp(char *s1, char *s2) 
+int wildcmp(char *s1, char *s2)
 {
-	
+
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0')
@@ -21,7 +21,7 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 		}
 	}
-	
+
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == *s2)
@@ -32,4 +32,4 @@ int wildcmp(char *s1, char *s2)
 	return (0);
 
 }
-	
+

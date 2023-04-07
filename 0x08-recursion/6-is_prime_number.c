c7092a93d76b99d3c2b2259d7dab035095a27e52
Manalhub <manalrahali24@gmail.com>
2023-04-06 17:17:09 -0700

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,13 +10,13 @@ int _prime_number(int i, int n)
 {
 	if (n <= 0)
 		return (0);
-	
+
 	if (n == 1)
 		return (0);
-	
+
 	if (n == 2)
 		return (0);
-	
+
 	if (i < n)
 	{
 		if (n % i == 0)
@@ -24,7 +24,7 @@ int _prime_number(int i, int n)
 
 		if ((n % i) != 0)
 		{
-			return (_prime_number(i+1, n));
+			return (_prime_number(i + 1, n));
 		}
 	}
 	return (1);
@@ -38,6 +38,7 @@ int _prime_number(int i, int n)
 int is_prime_number(int n)
 {
 	int i = 2;
+
 	if ((_prime_number(i, n)) == 0)
 		return (0);
 	return (1);
